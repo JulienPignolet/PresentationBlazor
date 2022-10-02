ac717397a24e578a1c8429acf61c546def001a08
Julien PIGNOLET <julien.pignolet@hotmail.fr>
2022-10-02 21:27:31 +0200

    Set up js interop

---
+++ PresentationBlazor/10.JsInterop/Pages/Index.razor.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace _10.JsInterop.Pages
+{
+    public partial class Index
+    {
+        [Inject]
+        public IJSRuntime? JSRuntime { get; set; }
+
+        private async Task Fireworks()
+        {
+            ArgumentNullException.ThrowIfNull(JSRuntime);
+            await JSRuntime.InvokeVoidAsync("Fireworks");
+        }
+    }
+}
---
+++ PresentationBlazor/10.JsInterop/npm/src/index.js
@@ -0,0 +1,15 @@
+﻿import fx from 'fireworks'
+
+window.Fireworks = () => {
+    let range = n => [...new Array(n)]
+
+    range(6).map(() =>
+        fx({
+            x: Math.random(10) + 200,
+            y: Math.random(10) + 200,
+            canvasWidth: window.innerWidth,
+            canvasHeight: window.innerHeight,
+            colors: ['#B243B6', '#F363B1', '#FDBF3B', '#F7F570', '#93EE81', '#47D4C4']
+        })
+    )
+}
