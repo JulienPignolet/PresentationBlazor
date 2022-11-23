f5dc714f9ba24ceb460307f623858e2f11911af7
Forandos <math.comandini@outlook.fr>
2022-11-23 18:02:02 +0100

    Share Data example

---
+++ PresentationBlazor/09.ShareData/09.ShareData/Pages/AppState.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace _09.ShareData.Pages;
+
+public class AppState
+{
+    public string SelectedColour { get; private set; }
+
+    public event Action OnChange;
+
+    public void SetColour(string colour)
+    {
+        SelectedColour = colour;
+        NotifyStateChanged();
+    }
+
+    private void NotifyStateChanged() => OnChange?.Invoke();
+}
--- PresentationBlazor/09.ShareData/09.ShareData/Program.cs
+++ PresentationBlazor/09.ShareData/09.ShareData/Program.cs
@@ -1,4 +1,5 @@
 using _09.ShareData.Data;
+using _09.ShareData.Pages;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 
@@ -8,6 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddSingleton<AppState>();
 
 var app = builder.Build();
 
